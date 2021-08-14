1fff8ba757a46a83c54383601351b7dd4344dd2d
Yerzhan Zhamashev <34035490+yerzhan0@users.noreply.github.com>
2021-08-14 23:13:30 +0300

    prod listen

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-const fastify = require("fastify")({ logger: true });
+const fastify = require("fastify")({
+  ignoreTrailingSlash: true,
+  logger: true,
+});
 const routes = require("./routes");
 const path = require("path");
 const DistPath = path.join(__dirname, "..", "dist");
@@ -21,6 +24,14 @@ fastify.get("/rule/:rule", async (request, reply) => {
 
 const start = async () => {
   try {
+    if (process.env.NODE_ENV == "production") {
+      await fastify.listen(3000, "0.0.0.0", (err, address) => {
+        if (err) {
+          fastify.log.error(err);
+          process.exit(1);
+        }
+      });
+    }
     await fastify.listen(3000);
   } catch (err) {
     fastify.log.error(err);
